96153b296ee6a3d17239f4a4adcb544a773d3db4
idn-fahmifn <fahmifn.idn@gmail.com>
2025-09-16 14:22:09 +0700

    2.2

--- resources/views/template/app.blade.php
+++ resources/views/template/app.blade.php
@@ -39,7 +39,24 @@
         <div class="card mt-4 p-2">
             <div class="card-body">
                 <h3>Judul Halaman</h3>
-                <span class="text-muted">subtitle halaman Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, omnis!</span>
+                <span class="text-muted">subtitle halaman Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Officiis, omnis!</span>
+            </div>
+        </div>
+        <div class="card mt-4 p-2">
+            <div class="card-body">
+                <div class="d-flex justify-content-between">
+                    <div class="card-title">
+                        <h5>Data Kategori</h5>
+                        <span class="text-muted">Berikut adalah tabel kategori produk</span>
+                    </div>
+                    <div class="">
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal"
+                            data-bs-target="#exampleModal">
+                            Tambah
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
